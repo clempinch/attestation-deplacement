6462a3d9e77db59d8205afdafc650f5b66f62bbb
Clement Pinchedez <c.pinchedez@criteo.com>
2020-11-03 12:35:46 +0100

    ✨ Store user settings in local storage on app startup

--- src/App.js
+++ src/App.js
@@ -1,47 +1,68 @@
 import React from 'react';
 import './App.css';
 import GenerationButton from './GenerationButton';
+import UserForm from './UserForm';
 
-// TODO: set this in local storage at webapp startup
-const settings = {
-  firstname: "Prénom",
-  lastname: "Nom",
-  birthday: "DD/MM/YYYY",
-  placeofbirth: "???",
-  address: "1 rue Lepic",
-  city: "Paris",
-  zipcode: "75001"
-};
-
+const userSettingKey = "userSettings";
 
 class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {
-      error: null,
-      isLoaded: false,
-      data: {},
-      loading: false
-    };
+    this.state = {};
   }
 
-  render() {
-    return (
-      <div className="App">
+  componentDidMount() {
+    const userSettings = localStorage.getItem(userSettingKey);
+    if (userSettings) {
+      this.setState({
+        userSettings: JSON.parse(userSettings),
+        userSettingsLoaded: true
+      });
+    } else {
+      this.setState({
+        userSettings: null,
+        userSettingsLoaded: false
+      });
+    }
+  }
+
+  storeSettings = (settings) => {
+    localStorage.setItem(userSettingKey, JSON.stringify(settings));
+    this.setState({
+      userSettings: settings,
+      userSettingsLoaded: true
+    });
+  }
+
+  getFormOrGenerationButtons() {
+    if (!this.state.userSettingsLoaded)
+      return (
         <header className="App-header">
-          <p>
-            Générer une attestation pour...
-          </p>
+          <UserForm onSubmit={(s) => this.storeSettings(s)}></UserForm>
+        </header>);
+    else {
+      const settings = this.state.userSettings;
+      return (
+        <header className="App-header">
+          <p>Générer une attestation pour...</p>
           <GenerationButton userSettings={settings} reason="sport_animaux" label="Se promener"></GenerationButton>
           <GenerationButton userSettings={settings} reason="travail" label="Se rendre au travail"></GenerationButton>
           <GenerationButton userSettings={settings} reason="achats" label="Faire des achats"></GenerationButton>
           <GenerationButton userSettings={settings} reason="sante" label="Se rendre à un RDV médical"></GenerationButton>
-        </header>
+        </header>)
+    }
+  }
+
+  render() {
+    const settings = this.state.userSettings;
+    return (
+      <div className="App">
+        {this.getFormOrGenerationButtons()}
       </div>
     );
   }
 
 }
 
-  export default App;
+export default App;
